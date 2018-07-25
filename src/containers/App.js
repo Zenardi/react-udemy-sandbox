a3b76f8e7f6f4d7f387a2d571665c6a12f975302
Eduardo Zenardi <zee8ca@bosch.com>
2018-07-25 13:54:42 -0300

    orginize project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-// import Validation from './Validation/Validation';
-// import Char from './Char/Char'
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -61,52 +60,20 @@ class App extends Component {
   }
 
   render() {
-    // const charList = this.state.userInput.split('').map((ch, index) => {
-    //   return <Char character={ch} key={index}
-    //     clicked={() => this.deleteCharHandler(index)} />
-    // });
-
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
-        </div>
+          <Persons persons={this.state.persons} clicked={this.deletePersonHandler} changed={this.nameChangedHandler}></Persons>
       );
-      btnClass = classes.red;
-
-    }
-
-    const assignedClasses =[];
-    if(this.state.persons.length <=2){
-      assignedClasses.push(classes.red);
-    }
-    if(this.state.persons.length <=1){
-      assignedClasses.push(classes.bold);
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button
-          className = {btnClass} 
-          onClick={this.togglePersonHandler}>Toggle Persons</button>
+        {<Cockpit showPersons={this.state.showPersons} 
+                  persons={this.state.persons}
+                  clicked={this.togglePersonHandler}/>}
         {persons}
-
-        {/* <input type='text' onChange={this.inputChangehandler} value={this.state.userInput} />
-        <p>{this.state.userInput}</p>
-        <Validation inputlenght={this.state.userInput.length} />
-        {charList} */}
       </div>
 
     );
